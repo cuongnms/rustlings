2aff7e18801b4d5a7f1b72f81cfa44c37d97f37f
cuongnm6 <cuongnm6@vng.com.vn>
2024-08-27 22:31:57 +0700

    add execrise

---
+++ exercises/clippy/target/debug/deps/clippy3-f48f6df6b709644c.d
@@ -0,0 +1,9 @@
+/home/lap14857/rustlings/exercises/clippy/target/debug/deps/libclippy3-f48f6df6b709644c.rmeta: clippy3.rs Cargo.toml
+
+/home/lap14857/rustlings/exercises/clippy/target/debug/deps/clippy3-f48f6df6b709644c.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
